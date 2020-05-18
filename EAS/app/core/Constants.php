daf2c202141ce69d6ed6a81bcda44cc916e4f4c3
kecoa queen <55182873+RizaruKecoak@users.noreply.github.com>
2020-05-18 10:46:28 +0700

    Update all

---
+++ EAS/app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define("BASE_URL", 'http://localhost/EAS/public');
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "eas_final");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
